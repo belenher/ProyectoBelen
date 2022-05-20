964982fe7aa7975b95d3c45a639c8ce9d9164c69
belenher <101005436+belenher@users.noreply.github.com>
2022-05-20 20:12:47 -0300

    Component

--- ProyectoBelen/src/app/app.module.ts
+++ ProyectoBelen/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducationComponent } from './components/education/education.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HsskillsComponent } from './components/hsskills/hsskills.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    ContactComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducationComponent,
+    HsskillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
