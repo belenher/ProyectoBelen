523aacf2a673329451a632538fcdd277dabd70cb
belenher <101005436+belenher@users.noreply.github.com>
2022-08-12 16:03:30 -0300

    backend carpeta

---
+++ ProyectoBelen/BackEnd/src/main/java/com/portfolio/belh/BelhApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.belh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelhApplication.class, args);
+	}
+
+}
